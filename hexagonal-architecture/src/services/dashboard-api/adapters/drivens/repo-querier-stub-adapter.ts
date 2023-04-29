a8a0111b8208961d3f9255b1b512f9057c3fe57e
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:26:26 -0300

    fix: repo querier adapter to use new external user type

--- hexagonal-architecture/src/services/dashboard-api/adapters/drivens/repo-querier-stub-adapter.ts
+++ hexagonal-architecture/src/services/dashboard-api/adapters/drivens/repo-querier-stub-adapter.ts
@@ -1,19 +1,19 @@
 import { ForRepositoryQuerying } from "../../ports/drivens/for-repository-querying";
-import { User as RepoUser } from "../../../repository/app/schemas/user";
+import { ExternalUser } from "../../../repository/app/schemas/user";
+import { User } from "../../app/schemas/user";
 
-const userMock: RepoUser = {
+const userMock: ExternalUser = {
   id: "1",
   name: "Lucas",
   email: "lucas@email.com",
 };
 
 export class RepoQuerierStub implements ForRepositoryQuerying {
-  getUser(_email: string): Promise<RepoUser> {
+  getUser(_email: string): Promise<ExternalUser> {
     return Promise.resolve(userMock);
   }
 
-  //TODO: type _user
-  createUser(_user: any, _password: string): Promise<RepoUser> {
+  createUser(_user: User): Promise<ExternalUser> {
     return Promise.resolve(userMock);
   }
 }
