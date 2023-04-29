7dce62ec7775f549e78f5de5af20e9a71fa1b49d
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 18:16:29 -0300

    fix: adding trpc to composition root

--- src/services/dashboard-api/app/composition-root.ts
+++ src/services/dashboard-api/app/composition-root.ts
@@ -1,5 +1,7 @@
+import { initTRPC } from "@trpc/server";
 import { ControlAuthenticatorStub } from "../adapters/drivens/control-authenticator-stub-adapter";
 import { RepoQuerierStub } from "../adapters/drivens/repo-querier-stub-adapter";
+import { authTRPCAdapter } from "../adapters/drivers/auth-trpc-adapter";
 import { AuthenticatorProxyAdapter } from "../adapters/drivers/authenticator-proxy";
 import { DashboardApi } from "./dashboard-api";
 
@@ -22,11 +24,28 @@ const compositionMock = () => {
 
 export const { authenticatorProxyAdapter } = compositionMock();
 
-console.log("Running Composition root...");
-const registerMock = {
-  name: "john",
-  email: "john@mal.com",
-  password: "1234",
+// console.log("Running Composition root...");
+// const registerMock = {
+//   name: "john",
+//   email: "john@mal.com",
+//   password: "1234",
+// };
+// authenticatorProxyAdapter.login("john@gmail.com", "1234");
+// authenticatorProxyAdapter.register(registerMock);
+
+export const localTRPCCompose = () => {
+  const controlAuthenticatorStub = new ControlAuthenticatorStub();
+  const repoQuerierStub = new RepoQuerierStub();
+  const dashboardApiMock = new DashboardApi(
+    controlAuthenticatorStub,
+    repoQuerierStub
+  );
+
+  const t = initTRPC.create();
+
+  const authTRPCAdapterRouter = authTRPCAdapter(dashboardApiMock, t);
+  const appRouter = t.router({
+    auth: authTRPCAdapterRouter,
+  });
+  return { appRouter };
 };
-authenticatorProxyAdapter.login("john@gmail.com", "1234");
-authenticatorProxyAdapter.register(registerMock);
