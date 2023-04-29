1ebd114b67894958fa50c6aedd6e23a4ae163a8f
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:29:24 -0300

    fix: removing split password when register on dashboard api

--- hexagonal-architecture/src/services/dashboard-api/app/dashboard-api.ts
+++ hexagonal-architecture/src/services/dashboard-api/app/dashboard-api.ts
@@ -1,9 +1,9 @@
 import { ForAuthenticating } from "../ports/drivers/for-authenticating";
 import { ForRepositoryQuerying } from "../ports/drivens/for-repository-querying";
-import { User as RepoUser } from "../../repository/app/schemas/user";
 import { ForControlAuthenticating } from "../ports/drivens/for-control-authenticating";
 import { AuthenticationDetails, Permissions } from "./schemas/auth";
 import { AuthenticatedUser, User } from "./schemas/user";
+import { RepoUser } from "../../repository/app/schemas/user";
 
 export class DashboardApi implements ForAuthenticating {
   constructor(
@@ -19,23 +19,22 @@ export class DashboardApi implements ForAuthenticating {
 
     const user: RepoUser = await this.repoQuerier.getUser(email);
 
-    const result = { ...user, ...authenticationDetails, permissions };
-    console.log("Login: ", result);
-    return result;
+    return { ...user, ...authenticationDetails, permissions };
   }
 
-  async register(user: User, password: string): Promise<AuthenticatedUser> {
-    const newUser = await this.repoQuerier.createUser(user, password);
+  async register(user: User): Promise<AuthenticatedUser> {
+    const newUser = await this.repoQuerier.createUser(user);
     const { email } = newUser;
 
     const authenticationDetails: AuthenticationDetails =
-      await this.controlAuthenticator.getAuthenticationDetails(email, password);
+      await this.controlAuthenticator.getAuthenticationDetails(
+        email,
+        user.password
+      );
 
     const permissions: Permissions =
-      await this.controlAuthenticator.getPermissions(email, password);
+      await this.controlAuthenticator.getPermissions(email, user.password);
 
-    const result = { ...newUser, ...authenticationDetails, permissions };
-    console.log("REGISTER: ", result);
-    return result;
+    return { ...newUser, ...authenticationDetails, permissions };
   }
 }
