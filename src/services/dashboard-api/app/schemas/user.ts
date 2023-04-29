c044b132afed98d0da6cf7de4bc74c80d76bcdf1
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 18:15:45 -0300

    add: register schema using zod to mutations

--- src/services/dashboard-api/app/schemas/user.ts
+++ src/services/dashboard-api/app/schemas/user.ts
@@ -1,12 +1,17 @@
-import { AuthenticatedDetailsSchema, PermissionsSchema } from "./auth";
+import { PermissionsSchema } from "./auth";
 import { z } from "zod";
+
+export const RegisterSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  password: z.string(),
+});
 
 export const AuthenticatedUserSchema = z.object({
   id: z.string(),
   email: z.string().email(),
   name: z.string(),
   // TODO: This should use AuthenticatedDetailsSchema, instead of duplicating token and refreshToken fields
-  authDetails: AuthenticatedDetailsSchema,
   permissions: PermissionsSchema,
 });
 
