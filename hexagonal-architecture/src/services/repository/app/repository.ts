5a6182b14d5aac80a832263d727962ea1c2d8a68
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:31:37 -0300

    add: repository base logic

---
+++ hexagonal-architecture/src/services/repository/app/repository.ts
@@ -0,0 +1,36 @@
+import { ForMonitoring } from "../ports/drivens/for-monitoring";
+import { ForManagingUser } from "../ports/drivers/for-managing-user";
+import { ExternalUser, RepoUser, User } from "./schemas/user";
+
+export class Repository implements ForManagingUser {
+  private userList: RepoUser[] = [];
+  constructor(private readonly logger: ForMonitoring) {}
+
+  async getUser(email: string): Promise<ExternalUser> {
+    const user = this.userList.find((user) => user.email === email);
+    if (!user) {
+      this.logger.log("Get User", "User not found");
+      throw new Error("User not found");
+    }
+    return user;
+  }
+
+  async createUser(user: User): Promise<ExternalUser> {
+    const userExist = this.userList.find((user) => user.email === user.email);
+    if (userExist) {
+      this.logger.log("Create User", "User already exists");
+      throw new Error("User already exists");
+    }
+
+    const newUser = {
+      ...user,
+      id: (this.userList.length + 1).toString(),
+    };
+
+    this.userList.push(newUser);
+    this.logger.log("Create User", "User created");
+
+    const { password, ...userWithoutPassword } = newUser;
+    return userWithoutPassword;
+  }
+}
