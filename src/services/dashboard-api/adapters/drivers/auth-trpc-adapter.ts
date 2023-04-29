df7eeaa016bb83a5251ad76a20ce8f9948c35a42
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 18:16:49 -0300

    add: setting auth trpc adapter

---
+++ src/services/dashboard-api/adapters/drivers/auth-trpc-adapter.ts
@@ -0,0 +1,23 @@
+import { initTRPC } from "@trpc/server";
+import { DashboardApi } from "../../app/dashboard-api";
+import {
+  AuthenticatedUserSchema,
+  RegisterSchema,
+} from "../../app/schemas/user";
+
+export function authTRPCAdapter(
+  dashboardApi: DashboardApi,
+  trpc: ReturnType<typeof initTRPC.create>
+) {
+  return trpc.router({
+    login: trpc.procedure
+      .input(RegisterSchema.pick({ email: true, password: true }))
+      .output(AuthenticatedUserSchema)
+      .mutation(({ input }) => dashboardApi.login(input.email, input.password)),
+
+    register: trpc.procedure
+      .input(RegisterSchema)
+      .output(AuthenticatedUserSchema)
+      .mutation(({ input }) => dashboardApi.register(input)),
+  });
+}
