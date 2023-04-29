b9647d3dcf17aa5dd443086ff1552de0d03dc237
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:25:53 -0300

    fix: user types on repo schema

--- hexagonal-architecture/src/services/repository/app/schemas/user.ts
+++ hexagonal-architecture/src/services/repository/app/schemas/user.ts
@@ -1,5 +1,11 @@
 export interface User {
-  id: string;
   name: string;
   email: string;
+  password: string;
 }
+
+export interface RepoUser extends User {
+  id: string;
+}
+
+export type ExternalUser = Omit<RepoUser, "password">;
