3aee40e070e5dea801cdb15e0dadf1893fca6176
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:29:46 -0300

    fix: dashboard api types user split from authenticated user

--- hexagonal-architecture/src/services/dashboard-api/app/schemas/user.ts
+++ hexagonal-architecture/src/services/dashboard-api/app/schemas/user.ts
@@ -9,4 +9,6 @@ export interface AuthenticatedUser {
   permissions: Permissions;
 }
 
-export type User = Pick<AuthenticatedUser, "email" | "name">;
+export interface User extends Pick<AuthenticatedUser, "email" | "name"> {
+  password: string;
+}
