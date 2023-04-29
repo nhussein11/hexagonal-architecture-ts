24231945d576058abf258f98dbb8598a52bfd730
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:31:21 -0300

    add: composition root to repository

---
+++ hexagonal-architecture/src/services/repository/app/composition-root.ts
@@ -0,0 +1,25 @@
+import { LoggerStubAdapter } from "../adapters/drivens/logger-stub-adapter";
+import { UserManagerProxy } from "../adapters/drivers/user-manager-proxy";
+import { Repository } from "./repository";
+
+export const compositionMock = () => {
+  const monitorStub = new LoggerStubAdapter();
+  const repositoryMock = new Repository(monitorStub);
+
+  const userManagerProxy = new UserManagerProxy(repositoryMock);
+
+  return { userManagerProxy };
+};
+
+export const { userManagerProxy } = compositionMock();
+
+const userMock = {
+  name: "lucas",
+  email: "lucas@mail.com",
+  password: "123456",
+};
+
+const { email: emailMock, password } = userMock;
+
+userManagerProxy.getUser(emailMock);
+userManagerProxy.createUser(userMock, password);
