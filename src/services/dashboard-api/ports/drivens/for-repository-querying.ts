b9b0c05ad617993f32e2471ff937394db9eceadd
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 15:44:42 -0300

    fix: for repo querying interace fixed to use external user instead of repo usr

--- src/services/dashboard-api/ports/drivens/for-repository-querying.ts
+++ src/services/dashboard-api/ports/drivens/for-repository-querying.ts
@@ -1,7 +1,7 @@
-import { RepoUser } from "../../../repository/app/schemas/user";
+import { ExternalUser } from "../../../repository/app/schemas/user";
 import { User } from "../../app/schemas/user";
 
 export interface ForRepositoryQuerying {
-  getUser(email: string): Promise<RepoUser>;
-  createUser(user: User): Promise<RepoUser>;
+  getUser(email: string): Promise<ExternalUser>;
+  createUser(user: User): Promise<ExternalUser>;
 }
