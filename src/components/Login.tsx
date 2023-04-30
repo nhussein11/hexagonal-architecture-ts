576763cfaf6671321ff1c023d2fd985c50b72308
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-30 16:14:11 -0300

    fix: password field linked to react hook on login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -30,6 +30,7 @@ const Login = () => {
           <input
             type="password"
             className="block py-2.5 px-0 w-full text-sm text-center text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            {...register("password")}
             placeholder="Password"
           />
         </div>
