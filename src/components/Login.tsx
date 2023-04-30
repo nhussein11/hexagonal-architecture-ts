75f4f9e055d18820a803617b82c1633ddba4258b
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-30 16:16:31 -0300

    fix: register forms styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,11 +16,12 @@ const Login = () => {
 
   return (
     <>
-      <h2 className="text-xl text-center">Login</h2>
+      <h2 className="text-2xl text-center">Login</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         {JSON.stringify(data)}
         <div className="relative z-0 w-full mb-6 group">
           <input
+            type="text"
             className="block py-2.5 px-0 w-full text-sm text-center text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             {...register("email")}
             placeholder="Email: "
@@ -34,6 +35,7 @@ const Login = () => {
             placeholder="Password"
           />
         </div>
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
