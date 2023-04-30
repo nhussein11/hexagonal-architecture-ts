1af6a2361d3d77f5d0ac5050812cfdaacc210a8e
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-29 21:56:49 -0300

    fix: wrapping main page with trpc provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import TRPCProvider from "./trpc.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <TRPCProvider>
+      <App />
+    </TRPCProvider>
+  </React.StrictMode>
+);
