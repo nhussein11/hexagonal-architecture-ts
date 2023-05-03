982cd0eb12fa310baefde70834c82e4ac6266765
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-03 16:22:03 -0300

    fix: update server  after refactor trpc compose

--- src/services/dashboard-api/server.ts
+++ src/services/dashboard-api/server.ts
@@ -1,7 +1,7 @@
 import * as trpcExpress from "@trpc/server/adapters/express";
 import cors from "cors";
 import express from "express";
-import { localTRPCCompose } from "./app/composition-root";
+import { TRPCCompose } from "./app/composition-root";
 import { PORT, TRPC_ENDPOINT } from "../../vite-env.d";
 
 // created for each request
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
-    router: localTRPCCompose().appRouter,
+    router: TRPCCompose().appRouter,
     createContext,
   })
 );
