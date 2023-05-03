0b93a251f268d21ec8185fe3a6c261a803fff3a5
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-03 16:20:10 -0300

    fix: forAuthenticating interface to avoid passing unnecessary separated password

--- src/services/dashboard-api/ports/drivers/for-authenticating.ts
+++ src/services/dashboard-api/ports/drivers/for-authenticating.ts
@@ -2,5 +2,5 @@ import { AuthenticatedUser, User } from "../../app/schemas/user";
 
 export interface ForAuthenticating {
   login: (email: string, password: string) => Promise<AuthenticatedUser>;
-  register: (user: User, password: string) => Promise<AuthenticatedUser>;
+  register: (user: User) => Promise<AuthenticatedUser>;
 }
