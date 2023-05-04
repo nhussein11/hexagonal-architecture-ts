a9e5eb2e7ef20a503e4da94126a5a4e14c0d35da
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-04 19:23:53 -0300

    fix: user manager proxy aligned with changes in for managing user

--- src/services/repository/adapters/drivers/user-manager-proxy.ts
+++ src/services/repository/adapters/drivers/user-manager-proxy.ts
@@ -4,8 +4,8 @@ import { ForManagingUser } from "../../ports/drivers/for-managing-user";
 
 export class UserManagerProxy implements ForManagingUser {
   constructor(private readonly repository: Repository) {}
-  getUser(email: string): Promise<ExternalUser> {
-    return Promise.resolve(this.repository.getUser(email));
+  getUser(email: string, password: string): Promise<ExternalUser> {
+    return Promise.resolve(this.repository.getUser(email, password));
   }
   createUser(user: User): Promise<ExternalUser> {
     return Promise.resolve(this.repository.createUser(user));
