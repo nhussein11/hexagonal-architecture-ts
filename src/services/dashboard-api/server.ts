e3d7c9f2744f5f13a0fde3faf79b407a21b47aaf
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-10 11:04:59 -0300

    fix: server properly exporting appRouter from composition root

--- src/services/dashboard-api/server.ts
+++ src/services/dashboard-api/server.ts
@@ -2,19 +2,17 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import cors from "cors";
 import express from "express";
 import { PORT, TRPC_ENDPOINT } from "../../vite-env.d";
-import { localTRPCCompose } from "./app/composition-root";
+import { appRouter } from "./app/composition-root";
 
 const createContext = () => ({});
 const app = express();
 
 app.use(cors());
 
-// const { appRouter } = TRPCCompose();
-
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
-    router: localTRPCCompose().appRouter,
+    router: appRouter,
     createContext,
   })
 );
