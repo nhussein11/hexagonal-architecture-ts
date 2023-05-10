e65449f9ced2f5bf6e2cd6e6ba6eefb20010f7f3
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-10 12:26:53 -0300

    add: generic inputs and button to register form

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import { trpc } from "../trpc";
-import Button from "./ui/Button";
+import { Button, Input } from "./ui";
 
 type RegisterForm = {
   email: string;
@@ -21,29 +21,24 @@ const Register = () => {
       <h2 className="text-2xl text-center">Register</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         {JSON.stringify(data)}
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            className="block py-2.5 px-0 w-full text-sm text-center text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            {...register("email")}
-            placeholder="Email: "
-          />
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="password"
-            className="block py-2.5 px-0 w-full text-sm text-center text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            {...register("password")}
-            placeholder="Password"
-          />
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="text"
-            className="block py-2.5 px-0 w-full text-sm text-center text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            {...register("name")}
-            placeholder="Name"
-          />
-        </div>
+        <Input<RegisterForm>
+          type="text"
+          placeholder="Email: "
+          registerItem="email"
+          register={register}
+        />
+        <Input<RegisterForm>
+          type="password"
+          placeholder="Password: "
+          registerItem="password"
+          register={register}
+        />
+        <Input<RegisterForm>
+          type="text"
+          placeholder="Name: "
+          registerItem="name"
+          register={register}
+        />
         <Button text="Register" type="submit" />
       </form>
     </>
