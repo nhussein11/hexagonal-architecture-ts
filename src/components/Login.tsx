619352920f80df34a7eba25443c5e5d93a823375
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-16 09:06:16 -0300

    fix: implementing modal body component  in login and register

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import useModal from "../hooks/useModal";
 import { trpc } from "../trpc";
 import { Button, Input, Modal } from "./ui";
 import { useState } from "react";
+import ModalBody from "./ui/ModalBody";
 
 type LoginForm = {
   email: string;
@@ -35,28 +36,16 @@ const Login = () => {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <div className="h-screen flex flex justify-center align-center">
-            <div className="flex flex-col items-center justify-center ">
-              <div className="mx-5 my-5 px-5 py-5 bg-zinc-600 rounded-lg shadow-xl">
-                <h2 className="font-mono font-bold text-3xl text-center ">
-                  Login Successful!
-                </h2>
-                <div className="flex flex-col items-center justify-center">
-                  <p className="font-mono font-bold text-3xl text-center ">
-                    {data?.name}
-                  </p>
-                  <p className="font-mono font-bold text-3xl text-center ">
-                    {data?.email}
-                  </p>
-                </div>
-                <Button
-                  text="Close"
-                  onClick={handleModal}
-                  className="my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                />
-              </div>
-            </div>
-          </div>
+          <ModalBody
+            title="Registration Successful!"
+            fields={[data?.name, data?.email]}
+          >
+            <Button
+              text="Close"
+              onClick={handleModal}
+              className="my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            />
+          </ModalBody>
         </Modal>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Input<LoginForm>
