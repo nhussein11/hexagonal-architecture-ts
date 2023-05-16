031c3743c4fb4488dece398715d6d2059a4393f8
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-16 08:54:42 -0300

    fix: isolating button classname

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,13 +2,19 @@ type ButtonProps = {
   type?: "submit" | "button" | "reset";
   text: string;
   onClick?: () => void;
+  className?: string;
 };
 
-const Button = ({ text, type, onClick }: ButtonProps) => {
-  const buttonClassName =
-    "my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+const buttonDefaultClassName =
+  "my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+
+const Button = ({ text, type, onClick, className }: ButtonProps) => {
   return (
-    <button type={type} className={buttonClassName} onClick={onClick}>
+    <button
+      type={type}
+      className={className ? className : buttonDefaultClassName}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
